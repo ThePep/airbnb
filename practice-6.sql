58fccc0c83a327a880727f3202887d3ed7e802f7
ThePep <jesse.laux@kellogg.northwestern.edu>
2022-01-14 15:30:24 +0000

    draft exercises 4-7

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,6 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT neighborhood, COUNT(*)
+FROM reviews INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY neighborhood;
